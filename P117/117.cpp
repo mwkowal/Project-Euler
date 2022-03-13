103c61a9e2f079b0b9fdfed1c6d437c61f458e0b
mwkowal <mwkowal@student.agh.edu.pl>
2022-03-13 18:29:11 +0100

    pokazanie

--- P117/117.cpp
+++ P117/117.cpp
@@ -3,13 +3,20 @@
 using namespace std;
 
 const long long B= 1000000007;
-const long long C= 100000001;
+const long long C= 1000000000001; //12
 vector<long long> result{1,1,2,4};
 
-void counting(){
-	for( long long i=4; i<C; i++){
-		result.push_back((result[i-1]%B+result[i-2]%B+result[i-3]%B+result[i-4]%B)%B);
+void counting(long long n){
+	long long a=1, b=1,c=2,d=4,e;
+	for( long long i=4; i<=n; i++){
+		e=d;
+		d=(a+b+c+d);
+		a=b; b=c; c=e; 
+		// result.push_back((result[i-1]%B+result[i-2]%B+result[i-3]%B+result[i-4]%B)%B);
+		// if (i % 10000000 == 0) // 7
+		// 	cout<<i<<" "<<result[i]<<"\n";
 	}
+	cout<<d<<"\n";
 		
 }
 
@@ -17,7 +24,12 @@ int main(){
 //	for(auto a : result)
 //
 //		cout<<a<<" ";
-	counting();
-	cout<<result[5]<<"\n";
+	int q; long long n;
+	cin >> q;
+	while(q--){
+		cin>>n;
+		counting(n);
+		// cout<<result[n]<<"\n";
+	}
 }
 
